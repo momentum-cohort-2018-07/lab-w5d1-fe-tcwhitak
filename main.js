85923a234b0eaf80152bf9cb030de6de62327056
tcwhitak <t.chad.whitaker@gmail.com>
2018-08-29 14:51:07 -0400

    bricks are on the page and disapper when hit with ball

--- main.js
+++ main.js
@@ -2,16 +2,25 @@
 // global variables for objects' references
 const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext('2d')
-const ballX = canvas.width / 2
-const ballY = canvas.height / 2
 const ballRadius = 10
-let deltaBallX = 1
-let deltaBallY = -1
+let deltaBallX = 2
+let deltaBallY = -2
 const paddleHeight = 10
 const paddleWidth = 75
 let paddleX = (canvas.width - paddleWidth) / 2
+const ballX = paddleX
+const ballY = canvas.height - paddleHeight
 let rightPressed = false
 let leftPressed = false
+let ballOnPaddle = true
+
+const brickRowCount = 3
+const brickColumnCount = 5
+const brickWidth = 75
+const brickHeight = 20
+const brickPadding = 10
+const brickOffsetTop = 30
+const brickOffsetLeft = 30
 
 class Game {
   constructor () {
@@ -22,28 +31,54 @@ class Game {
       height: this.canvas.height }
     this.paddle = new Paddle(this, paddleHeight, paddleWidth, paddleX)
     this.ball = new Ball(this, deltaBallX, deltaBallY, ballX, ballY, this.paddle)
+    this.bricks = []
+    for (let c = 0; c < brickColumnCount; c++) {
+      for (let r = 0; r < brickRowCount; r++) {
+        const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft
+        const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop
+        this.bricks.push(new Brick(this, brickX, brickY))
+      }
+    }
     this.ticks = 0
     let tick = () => {
       this.ticks++
-      this.update()
       this.draw()
-      document.addEventListener('keydown', keyDownHandler, false)
-      document.addEventListener('keyup', keyUpHandler, false)
       window.requestAnimationFrame(tick)
     }
     this.tick = tick
+
+    document.addEventListener('keydown', keyDownHandler, false)
+    document.addEventListener('keyup', keyUpHandler, false)
   }
-  update () {
-    console.log('update')
-  }
+
   draw () {
+    // check for brick collisions
+    this.checkBrickCollision()
     // clear frame to remove past drawings
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // draw ball, updates position after every draw in function
-    this.ball.draw()
     // draw paddle
     this.paddle.draw()
+    // draw ball, updates position after every draw in function
+    this.ball.draw()
+    for (let brick of this.bricks) {
+      brick.draw()
+    }
   }
+
+  checkBrickCollision () {
+    for (let b of this.bricks) {
+      if (this.ball.Pos.x > b.Pos.x &&
+          this.ball.Pos.x < b.Pos.x + brickWidth &&
+          this.ball.Pos.y > b.Pos.y &&
+          this.ball.Pos.y < b.Pos.y + brickHeight) {
+        deltaBallY = -deltaBallY
+        b.hit = true
+      }
+    }
+
+    this.bricks = this.bricks.filter(brick => !brick.hit)
+  }
+
   start () {
     this.tick()
   }
@@ -64,6 +99,13 @@ class Ball {
     ctx.fillStyle = '#17A398'
     ctx.fill()
     ctx.closePath()
+
+    if (ballOnPaddle) {
+      this.Pos.x = this.game.paddle.Pos.x + paddleWidth / 2
+      this.Pos.y = canvas.height - paddleHeight
+      return
+    }
+
     // if next position hits X edges then change deltaBallX
     if (this.Pos.x + deltaBallX > canvas.width - ballRadius ||
         this.Pos.x + deltaBallX < ballRadius) {
@@ -76,7 +118,7 @@ class Ball {
       if (this.Pos.x > this.paddle.Pos.x && this.Pos.x < this.paddle.Pos.x + paddleWidth) {
         deltaBallY = -deltaBallY
       } else {
-        window.alert('game over')
+        // window.alert('game over')
         document.location.reload()
       }
     }
@@ -105,11 +147,27 @@ class Paddle {
   }
 }
 
+class Brick {
+  constructor (game, x, y) {
+    this.Pos = {x: x, y: y}
+    this.hit = false
+  }
+  draw () {
+    ctx.beginPath()
+    ctx.rect(this.Pos.x, this.Pos.y, brickWidth, brickHeight)
+    ctx.fillStyle = '#0095DD'
+    ctx.fill()
+    ctx.closePath()
+  }
+}
+
 function keyDownHandler (e) {
   if (e.keyCode === 39) {
     rightPressed = true
   } else if (e.keyCode === 37) {
     leftPressed = true
+  } else if (e.keyCode === 32) {
+    ballOnPaddle = false
   }
 }
 
